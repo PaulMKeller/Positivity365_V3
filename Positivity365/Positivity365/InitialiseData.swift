d99513afeb978ddbeb0bf7aefc11e9badc33f8d2
Paul Keller <paul.keller.it@gmail.com>
2017-01-24 19:19:53 +0800

    Adding NSManaged objects for Core Data Use

--- Positivity365/Positivity365/InitialiseData.swift
+++ Positivity365/Positivity365/InitialiseData.swift
@@ -14,14 +14,71 @@ class InitialiseData{
         
     }
     
-    class func setupData() -> Bool {
-        return false
+    class func checkForData() {
+        if checkYears() == false {
+            createYears(newYearGroup: "2017", newLeapYear: false)
+            createYears(newYearGroup: "2018", newLeapYear: false)
+            createYears(newYearGroup: "2019", newLeapYear: false)
+            createYears(newYearGroup: "2020", newLeapYear: true)
+            
+            createMonths(newMonthGroup: "January", newMaxDay: 31)
+            createMonths(newMonthGroup: "February", newMaxDay: 29)
+            createMonths(newMonthGroup: "March", newMaxDay: 31)
+            createMonths(newMonthGroup: "April", newMaxDay: 30)
+            createMonths(newMonthGroup: "May", newMaxDay: 31)
+            createMonths(newMonthGroup: "June", newMaxDay: 30)
+            createMonths(newMonthGroup: "July", newMaxDay: 31)
+            createMonths(newMonthGroup: "August", newMaxDay: 31)
+            createMonths(newMonthGroup: "September", newMaxDay: 30)
+            createMonths(newMonthGroup: "October", newMaxDay: 31)
+            createMonths(newMonthGroup: "November", newMaxDay: 30)
+            createMonths(newMonthGroup: "December", newMaxDay: 31)
+            
+            for i: Int16 in 1...31 {
+                createDays(newDay: i)
+            }
+            
+            DataController.saveContext()
+        }
+    }
+    
+    class func checkYears() -> Bool {
+        let fetchRequest:NSFetchRequest<Year> = Year.fetchRequest()
         
-        /*
-        * Check if the data already exists
-         * Check for data in Year, Month, Day entities
-        * If not set it up
-        * if it does then bomb out, nothing to check
-        */
+        do {
+            let searchResults = try DataController.getContext().fetch(fetchRequest)
+            
+            if searchResults.count <= 0 {
+                return false
+            } else {
+                return true
+            }
+        } catch {
+            print("Error: \(error)")
+            return false
+        }
+    }
+    
+    class func createYears(newYearGroup: String, newLeapYear: Bool) {
+        let yearClassName:String! = String(describing: Year.self)
+        
+        let year:Year = NSEntityDescription.insertNewObject(forEntityName: yearClassName, into: DataController.getContext()) as! Year
+        year.yearGroup = newYearGroup
+        year.leapYear = newLeapYear
+    }
+    
+    class func createMonths(newMonthGroup: String, newMaxDay: Int16) {
+        let monthClassName:String! = String(describing: Month.self)
+        
+        let month:Month = NSEntityDescription.insertNewObject(forEntityName: monthClassName, into: DataController.getContext()) as! Month
+        month.monthGroup = newMonthGroup
+        month.maxDay = newMaxDay
+    }
+    
+    class func createDays(newDay: Int16) {
+        let dayClassName:String! = String(describing: Day.self)
+        
+        let day:Day = NSEntityDescription.insertNewObject(forEntityName: dayClassName, into: DataController.getContext()) as! Day
+        day.day = newDay
     }
 }

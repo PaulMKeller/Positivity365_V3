6b7425a22469accae69e37ef6f43e7ad88150be0
Paul Keller <paul.keller.it@gmail.com>
2017-03-22 16:18:22 +0800

    Adding positive thoughts view

--- Positivity365/Positivity365/HTMLFunctions.swift
+++ Positivity365/Positivity365/HTMLFunctions.swift
@@ -21,16 +21,63 @@ class HTMLFunctions {
         
         currentHTML = "<HTML>"
         currentHTML += "<HEAD>"
-        currentHTML += "<link rel='stylesheet' href='../main.css'>"
+        currentHTML += setCSS()
         currentHTML += "</HEAD>"
         currentHTML += "<BODY>"
-        currentHTML += "Some positive nonsence goes here - Titty McKaka"
+        currentHTML += setQuote()
         currentHTML += "</BODY>"
         currentHTML += "</HTML>"
+        
+        print(currentHTML)
         
         return currentHTML
     }
     
+    static func setCSS() -> String {
+    
+        var myCSS: String
+    
+        myCSS = "<style type='text/css'>"
+        
+        
+        myCSS += "body {"
+        myCSS += "   font-family: cursive;"
+        myCSS += "   font-size: x-large;"
+        myCSS += "   font-weight: bold;"
+        myCSS += "}"
+        
+        myCSS += ".quote {"
+        myCSS += "   float: center;"
+        myCSS += "}"
+        
+        
+        myCSS += "</style>"
+    
+        return myCSS
+
+    }
+    
+    static func setQuote() -> String{
+        
+        var myQuote: String
+        
+        myQuote = "<div id='quote'>"
+        myQuote += "Some positive nonsence goes here - Titty McKaka"
+        myQuote += "</div>"
+        
+        return myQuote
+    }
+    
+    static func setBackgroundImage() -> String {
+        var myBackgroundImage: String
+        
+        myBackgroundImage = "<div id='backgroudImage'>"
+        myBackgroundImage = "</div>"
+        
+        return myBackgroundImage
+    }
+    
+    /*
     private func getQuotes(){
         let fetchRequest:NSFetchRequest<PositiveThought> = PositiveThought.fetchRequest()
         
@@ -50,4 +97,5 @@ class HTMLFunctions {
         }
 
     }
+    */
 }
